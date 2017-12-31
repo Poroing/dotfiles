d8cf0a8b6bdc7a54ce5c45ad11427fea402b6301
Poroing <jean-18-09@hotmail.fr>
2017-12-31 09:36:03 +0100

    Moved some script in the stow

--- graphical/.config/personal/battery_low_alert.sh
+++ graphical/.config/personal/battery_low_alert.sh
@@ -8,12 +8,12 @@ source /home/jean/.config/colors.sh
 minimum_battery_level=10
 
 battery_level () {
-    level=$(acpi -b | awk '{ print $4 }')
+    level=$(acpi -b | awk '/Battery 0/ { print $4 }')
     echo -n ${level%"%,"}
 }
 
 battery_state () {
-    state=$(acpi -b | awk '{ print $3 }')
+    state=$(acpi -b | awk '/Battery 0/ { print $3 }')
     state=${state%,}
     echo -n $state
 }
@@ -35,4 +35,4 @@ battery_alert_text=$(echo -n "\
 ######################")
 
 
-/home/jean/Programation/Scripts/alert.sh -b "$background_color" -f "$foreground_color" "$battery_alert_text"
+/home/jean/.config/personal/alert.sh -b "$background_color" -f "$foreground_color" "$battery_alert_text"
