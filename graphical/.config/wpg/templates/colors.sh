8ece30cb56ed252a3443d751a9a5c2b6c119d41b
Jean Jouve <jean.jouve@protonmail.com>
2021-02-15 11:16:21 +0100

    Changed the way wpg's templates are stored to allow direct installation with stow

--- graphical/.config/wpg/templates/colors.sh
+++ graphical/.config/wpg/templates/colors.sh
@@ -1,1 +1,25 @@
-/home/jean/.config/colors.sh
+case $1 in
+    white) echo "#a4c6f1";;
+    light_grey) echo "#8fadd3";;
+
+    red) echo "#9A6E52";;
+    light_red) echo "#804825";;
+
+    red_yellow) echo "#7D816D";;
+    light_red_yellow) echo "#636e37";;
+
+    green) echo "#D7A961";;
+    light_green) echo "#b17c28";;
+
+    blue) echo "#516C8C";;
+    light_blue) echo "#254975";;
+
+    light_violet) echo "#98966D";;
+    very_light_violet) echo "#807d35";;
+
+    cyan) echo "#73879A";;
+    light_cyan) echo "#395f83";;
+
+    dark_grey)echo "#0a0c0c";;
+    black) echo "#010101";;
+esac
