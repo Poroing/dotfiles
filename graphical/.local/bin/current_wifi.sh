16bc50083abb28af317e11edb1994b8cc73a0896
Jean Jouve <jean.jouve@protonmail.com>
2021-02-15 11:17:23 +0100

    Now using iwctl

--- graphical/.local/bin/current_wifi.sh
+++ graphical/.local/bin/current_wifi.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-current_wifi=$(netctl-auto list | awk '/^*/ { print $2 }')
+AWK_SCRIPT='
+/Connected/ {
+    for (i = 3; i <= NF; i++) {
+        print $i
+    }
+}'
+
+current_wifi=$(iwctl station wlan0 show | awk "$AWK_SCRIPT")
 if [[ "$current_wifi" == "" ]]; then
     current_wifi="None"
 fi
